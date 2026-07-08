d60e5f75b8ea38d2a4ceffcb64b19a740557ffac
Sumaiyaakter98 <sa9075@gmail.com>
2026-07-08 16:32:29 +0600

    Fix duplicate name property in User schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -15,10 +15,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    name: {
-      type: String,
-      required: true,
-    },
     role: {
       type: String,
       enum: ["user", "admin"],
